bcf396386f3d2b4b597650b7c3ba5d682dbb7c24
AlexWebArt <elusiv@mail.ru>
2025-06-24 12:11:20 +0500

    T-1: project init

---
+++ src/pages/home/homePage.tsx
@@ -0,0 +1,9 @@
+import style from './homePage.module.css'
+
+export const HomePage = () => {
+    return (
+        <main>
+            <h1 className={style.page_title}>Home page</h1>
+        </main>
+    )
+}
