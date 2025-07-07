38ecbe13b1fefbc7a5012a69c8720a1a89428ab3
AlexWebArt <elusiv@mail.ru>
2025-07-07 21:49:31 +0500

    T-11: added skeleton loader

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -3,11 +3,25 @@ import { FlightDetails } from 'components/flightDetails'
 import { FlightList } from 'components/flightList'
 import flights from 'shared/data/flights.json'
 import styles from './homePage.module.css'
+import { useEffect, useState } from 'react'
+import { SkeletonLoader } from 'shared/ui/skeletonLoader'
 
 export const HomePage = () => {
+    const [isFlightLoading, setIsFlightLoading] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => setIsFlightLoading(false), 1500)
+
+        return () => clearTimeout(timer)
+    }, [])
+
     return (
         <div className={styles.page_layout}>
-            <FlightList flights={flights} />
+            {
+                isFlightLoading ?
+                    <SkeletonLoader count={5} /> :
+                    <FlightList flights={flights} />
+            }
             <Header />
             <FlightDetails flights={flights} />
         </div>
---
+++ src/shared/ui/skeletonLoader/skeletonLoader.tsx
@@ -0,0 +1,23 @@
+import styles from './skeletonLoader.module.css'
+
+interface Props {
+    count: number
+}
+
+export function SkeletonLoader({ count }: Props) {
+    return (
+        <div className={styles.skeleton_list}>
+            <div className={styles.skeleton_filter}>
+
+            </div>
+            {
+                Array.from({ length: count }, (_, index) => (
+                    <div key={index} className={styles.skeleton_card}>
+
+                    </div>
+                ))
+            }
+        </div>
+
+    )
+}
