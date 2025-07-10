82240f3905771d20bfe525e50093cf851b9417ea
AlexWebArt <elusiv@mail.ru>
2025-07-11 02:03:14 +0500

    T-14: added second filters for airlines

--- src/components/flightList/flightFilters/flightFilters.tsx
+++ src/components/flightList/flightFilters/flightFilters.tsx
@@ -1,33 +1,55 @@
-import styles from './flightFilters.module.css'
+import type { IFlightFilters } from "../flightList";
+import styles from "./flightFilters.module.css";
 
 interface Props {
-    filters: Set<string>
-    activeFilter: string
-    setActiveFilter: (filter: string) => void
+  filteredCountries: Set<string>;
+  filteredAirlines: Set<string>;
+  activeFilter: IFlightFilters;
+  setActiveFilter: (filters: IFlightFilters) => void;
 }
 
-export function FlightFilters({ filters, activeFilter, setActiveFilter }: Props) {
-
-    return (
-        <div className={styles.outer_container}>
-            <select
-                className={styles.filters_select}
-                name="filters"
-                value={activeFilter}
-                onChange={e => setActiveFilter(e.target.value)}
-            >
-                {
-                    Array.from(filters).map(filter => (
-                        <option
-                            key={filter}
-                            className={styles.filter_option}
-                            value={filter}
-                        >
-                            {filter}
-                        </option>))
-                }
-            </select>
-        </div>
-
-    )
+export function FlightFilters({
+  filteredCountries,
+  filteredAirlines,
+  activeFilter,
+  setActiveFilter,
+}: Props) {
+  return (
+    <div className={styles.outer_container}>
+      <select
+        className={styles.filters_select}
+        name="filterCountries"
+        value={activeFilter.countrie}
+        onChange={(e) =>
+          setActiveFilter({
+            ...activeFilter,
+            countrie: e.target.value,
+          })
+        }
+      >
+        {Array.from(filteredCountries).map((filter) => (
+          <option key={filter} className={styles.filter_option} value={filter}>
+            {filter}
+          </option>
+        ))}
+      </select>
+      <select
+        className={styles.filters_select}
+        name="filterAirlines"
+        value={activeFilter.airline}
+        onChange={(e) =>
+          setActiveFilter({
+            ...activeFilter,
+            airline: e.target.value,
+          })
+        }
+      >
+        {Array.from(filteredAirlines).map((filter) => (
+          <option key={filter} className={styles.filter_option} value={filter}>
+            {filter}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
--- src/components/flightList/flightList.tsx
+++ src/components/flightList/flightList.tsx
@@ -1,29 +1,66 @@
-import { useState } from "react"
-import type { IFlight } from "shared/types/IFlight.interface"
-import { FlightCard } from "../flightCard"
-import { FlightFilters } from "./flightFilters/flightFilters"
-import styles from './flightList.module.css'
-
+import { useState } from "react";
+import type { IFlight } from "shared/types/IFlight.interface";
+import { FlightCard } from "../flightCard";
+import { FlightFilters } from "./flightFilters/flightFilters";
+import styles from "./flightList.module.css";
 
 interface Props {
-    flights: IFlight[]
+  flights: IFlight[];
+}
+
+export interface IFlightFilters {
+  countrie: string;
+  airline: string;
 }
 
 const getFilteredCountries = (flights: IFlight[]) => {
-    return new Set(['All', ...flights.map(flight => flight.route.countryName)])
-}
+  return new Set(["All", ...flights.map((flight) => flight.route.countryName)]);
+};
+
+const getFilteredAirlines = (flights: IFlight[]) => {
+  return new Set(["All", ...flights.map((flight) => flight.airline)]);
+};
 
 export function FlightList({ flights }: Props) {
-    const [activeFilter, setActiveFilter] = useState('All')
-    const filteredCountries = getFilteredCountries(flights)
-    const filteredFlight = activeFilter === 'All' ? flights : flights.filter(flight => flight.route.countryName === activeFilter )
+  const [activeFilters, setActiveFilter] = useState<IFlightFilters>({
+    countrie: "All",
+    airline: "All",
+  });
+  const filteredCountries = getFilteredCountries(flights);
+  const filteredAirlines = getFilteredAirlines(flights);
+  const filteredFlight =
+    activeFilters.countrie === "All" && activeFilters.airline === "All"
+      ? flights
+      : flights.filter((flight) => {
+          if (
+            activeFilters.countrie !== "All" &&
+            activeFilters.airline === "All"
+          ) {
+            return flight.route.countryName === activeFilters.countrie;
+          }
+          if (
+            activeFilters.airline !== "All" &&
+            activeFilters.countrie === "All"
+          ) {
+            return flight.airline === activeFilters.airline;
+          }
+          return (
+            flight.route.countryName === activeFilters.countrie &&
+            flight.airline === activeFilters.airline
+          );
+        });
 
-    return (
-        <div className={styles.flight_list}>
-            <FlightFilters filters={filteredCountries} activeFilter={activeFilter} setActiveFilter={setActiveFilter}/>
-            {
-                filteredFlight.map(flight => <FlightCard flight={flight} key={flight.flightNumber} />)
-            }
-        </div>
-    )
+  return (
+    <div className={styles.flight_list}>
+      <FlightFilters
+        filteredCountries={filteredCountries}
+        filteredAirlines={filteredAirlines}
+        activeFilter={activeFilters}
+        setActiveFilter={setActiveFilter}
+      />
+      {filteredFlight.map((flight) => (
+        <FlightCard flight={flight} key={flight.flightNumber} />
+      ))}
+    </div>
+  );
 }
